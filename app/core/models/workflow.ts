afc32ca8375c9dabf1c65940ae13ac8357423fc8
EnoF <andyt@live.nl>
2015-05-20 10:07:44 +0200

    added models from workflow downwards

--- app/core/models/workflow.ts
+++ app/core/models/workflow.ts
@@ -1,11 +1,19 @@
 module Models {
   export class Workflow extends Serializable {
-    constructor(json: Object) {
+    _id: string;
+    name: string;
+    calls: Array<any> = [];
+    connectors: Array<any> = [];
+
+    constructor(json: IWorkflow) {
       super(json);
+      this.calls = this.convertContentToClass(json.calls, Call);
+      this.connectors = this.convertContentToClass(json.connectors, Connector);
     }
   }
 
   export interface IWorkflow {
+    _id: string;
     name: string;
     calls: Array<string>;
     connectors: Array<string>;
