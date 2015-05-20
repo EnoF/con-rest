aa9307a3c94c266d5fe9fa5bf51d50b9e52ea551
EnoF <andyt@live.nl>
2015-05-20 15:08:00 +0200

    workflow overview loading implemented

--- app/core/dao/workflowDAO.ts
+++ app/core/dao/workflowDAO.ts
@@ -1,13 +1,26 @@
 module DAO {
   import IInjectorService = ng.auto.IInjectorService;
+  import Workflow = Models.Workflow;
 
   export class WorkflowDAO extends DAO {
     constructor($injector: IInjectorService) {
       super($injector);
     }
 
-    getAll() {
-      return this.get('/api/workflows/', null);
+    getAll(): ng.IPromise<Array<Workflow>> {
+      var deferred = this.$q.defer();
+      this.get('/api/workflows/', null)
+        .then((response: any) => {
+          deferred.resolve(new Workflow(response.data));
+        }, deferred.reject);
+      return deferred.promise;
     }
   }
+
+  var instance = null;
+
+  export function workflowDAO($injector: IInjectorService) {
+    return instance = new WorkflowDAO($injector);
+  }
+  workflowDAO.$inject = ['$injector'];
 }
