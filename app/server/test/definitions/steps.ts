1f98c3d4e0bffab4835111f6de34394251df7eaa
EnoF <andyt@live.nl>
2015-06-19 10:16:43 +0200

    workflow view single spec added

--- app/server/test/definitions/steps.ts
+++ app/server/test/definitions/steps.ts
@@ -16,6 +16,14 @@ library
       method: method,
       type: type
     }).then(() => done()))
+  .given('<Workflow><Name><Calls>', (done) => done())
+  .given('<Workflow><(.*)><(.*)>', (name: string, callsCSV: string, done) => {
+    mongoose.model('Workflow').create({
+      _id: new ObjectId(globals.createIdBasedOnName(name)),
+      name: name,
+      calls: globals.convertCSVToIds(callsCSV)
+    }).then(() => done());
+  })
   .when('I view call "(.*)"', (name: string, done) => {
     supertest('http://localhost:1111')
       .get('/api/requests/' + globals.createIdBasedOnName(name))
@@ -27,6 +35,51 @@ library
   })
   .then('I expect to see "(.*)" as (.*)', (url: string, prop: string, done) => {
     expect(this.result.body[prop]).to.equal(url);
+        this.inspect = res.body;
+        expect(res.status).to.equal(200);
+        done();
+      });
+  })
+  .when('I view workflow "(.*)"', (name: string, done) => {
+    supertest('http://localhost:1111')
+      .get('/api/workflows/' + globals.createIdBasedOnName(name))
+      .end((req, res) => {
+        this.inspect = res.body;
+        expect(res.status).to.equal(200);
+        done();
+      });
+  })
+  .when('I view all calls', (done) => {
+    supertest('http://localhost:1111')
+      .get('/api/requests/')
+      .end((req, res) => {
+        this.result = res.body;
+        expect(res.status).to.equal(200);
+        done();
+      });
+  })
+  .when('I inspect call (.*)', (pos: string, done) => {
+    this.inspect = this.result[parseInt(pos, 10)];
+    done();
+  })
+  .then('I expect to see "(.*)" as (.*)', (value: string, prop: string, done) => {
+    expect(this.inspect[prop]).to.equal(value);
+    done();
+  })
+  .then('I expect to see ids "(.*)" as (.*)', (idsCSV: string, prop: string, done) => {
+    var ids = globals.convertCSVToIds(idsCSV);
+    var stringIds = [];
+    this.inspect[prop].forEach((id) => {
+      if (!!id._id) {
+        stringIds.push(id._id);
+      } else {
+        stringIds.push(id);
+      }
+    });
+    expect(this.inspect[prop].length).to.equal(ids.length);
+    ids.forEach((id) => {
+      expect(stringIds).to.include(id.toString());
+    });
     done();
   });
 
