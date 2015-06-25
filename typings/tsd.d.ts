fd9a99c2ed285ef325d412f56fec87aa14def508
EnoF <andyt@live.nl>
2015-06-25 11:08:04 +0200

    request execution implemented

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -16,3 +16,5 @@
 /// <reference path="superagent/superagent.d.ts" />
 /// <reference path="request/request.d.ts" />
 /// <reference path="form-data/form-data.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
+/// <reference path="q/Q.d.ts" />
